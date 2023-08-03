23324b1cc8749c1a732c0b513c062105c6977e0f
Ansh Kushwaha <ansh.kushwaha26@gmail.com>
2023-08-03 20:12:41 +0530

    #1 Add Icon & UI Changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,19 @@ import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession();
+  const name: string | null | undefined = session?.user?.name;
 
   return (
-    <div className="w-1/2 min-w-[320px] mx-auto mt-4 bg-slate-50 rounded-lg shadow-lg">
+    <div className="w-1/2 min-w-[320px] h-fit max-h-full mx-auto mt-4 bg-slate-50 rounded-lg shadow-lg">
       <div className="flex flex-col text-center my-5 gap-4">
-        <h1 className="text-2xl font-bold text-black">Todo List</h1>
-        <AddTask />
-        <TodoList />
+        <h1 className="text-2xl font-bold text-black">
+          {session && `${name?.split(' ').slice(0, 1)}'s `}
+          Todo List
+        </h1>
+        <div className='pb-4'>
+          <AddTask />
+        </div>
+          <TodoList />
       </div>
     </div>
   )
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,16 @@
 'use client'
 
 import { signOut } from "next-auth/react"
+import Image from "next/image"
 import { GiHamburgerMenu } from 'react-icons/gi'
 
 function NavBar() {
   return (
-    <div className="h-12 w-full flex flex-row justify-between px-2 items-center bg-gray-100 rounded-b-lg shadow-lg">
-        <div className='text-2xl font-sans font-bold text-gray-800'>Todo App</div>
+    <div className="h-12 w-full flex flex-row justify-between px-4 items-center bg-gray-100 rounded-b-lg shadow-lg">
+        <div className='flex flex-row items-center text-2xl font-sans font-bold text-gray-800'>
+          <Image src = 'icon.svg' width={32} height={32} alt="icon" />
+          {/* Todo App */}
+        </div>
         <button className="btn btn-sm btn-square btn-neutral btn-outline text-gray-50"
           onClick={() => signOut()}
         >
