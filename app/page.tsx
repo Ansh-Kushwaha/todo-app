f90c7aeafd16741cc7ee88ed6c13684999c6144b
Ansh Kushwaha <ansh.kushwaha26@gmail.com>
2023-08-05 13:29:28 +0530

    Dark Mode fixes

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const name: string | null | undefined = session?.user?.name;
 
   return (
-    <div className="w-1/2 min-w-[320px] h-fit max-h-full mx-auto mt-4 bg-slate-50 dark:bg-[#121212] rounded-lg shadow-lg">
+    <div className="w-1/2 min-w-[320px] h-fit max-h-full mx-auto mt-4 bg-slate-50 dark:bg-[#121212] rounded-lg shadow-lg dark:shadow-[#4a4a4a1f]">
       <div className="flex flex-col text-center my-5 gap-4">
         <h1 className="text-2xl font-bold text-gray-800 dark:text-slate-100">
           {session && `${name?.split(' ').slice(0, 1)}'s `}
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -10,7 +10,7 @@ function NavBar() {
   const { data: session } = useSession();
 
   return (
-    <div className="h-12 w-full flex flex-row justify-between px-4 items-center bg-gray-100 dark:bg-[#121212] backdrop-blur-sm rounded-b-lg shadow-lg dark:shadow-sm dark:shadow-[#4a4a4a]">
+    <div className="h-12 w-full flex flex-row justify-between px-4 items-center bg-gray-100 dark:bg-[#121212] rounded-b-lg shadow-lg dark:shadow-[#4a4a4a1f]">
         <div className='flex flex-row items-center text-2xl font-sans font-bold text-gray-800'>
           <Image src = 'icon.svg' width={32} height={32} alt="icon" />
           {/* Todo App */}
@@ -21,16 +21,16 @@ function NavBar() {
           <GiHamburgerMenu className='h-6 w-6 fill-gray-600 dark:fill-gray-300' />
         </button>
         {showMenu && 
-        <div className='absolute top-[64px] right-[10px] flex flex-col items-center rounded-md w-[120px] h-fit shadow-lg bg-slate-100 dark:bg-[#121212] z-10'>
+        <div className='absolute top-[64px] right-[10px] flex flex-col items-center rounded-md w-[120px] h-fit shadow-lg dark:shadow-[#4a4a4a1f] bg-slate-100 dark:bg-[#121212] z-999'>
           <div>
             {session && 
-            <img src={session?.user?.image!} width={64} height={64} className='mt-4 rounded-full hover:cursor-pointer' />
+            <img src={session?.user?.image!} width={64} height={64} className='mt-4 rounded-full hover:cursor-pointer z-999' />
             }
           </div>
-          <div className='flex justify-center items-center text-sm font-semibold text-gray-800 dark:text-slate-50'>
+          <div className='flex justify-center items-center text-sm font-semibold text-gray-800 dark:text-slate-50 z-999'>
             {session && session?.user?.name}
           </div>
-          <button className='btn btn-neutral btn-sm text-gray-50 text-sm my-5'
+          <button className='btn btn-neutral btn-sm text-gray-50 text-sm my-5 z-999'
             onClick={() => {signOut()}}
           >
             Logout
