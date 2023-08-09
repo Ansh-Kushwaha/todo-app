0b433fcda25f9745c73fbd0bd16d413aaa8dd89a
Ansh Kushwaha <ansh.kushwaha26@gmail.com>
2023-08-09 14:47:58 +0530

    UI Updates

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -5,11 +5,11 @@ import Image from 'next/image';
 
 function Login() {
   return (
-    <div className=" bg-white text-gray-800 h-screen flex flex-col items-center justify-center text-center">
-        {/* <Image src={icon} alt="ChatGPT 2.0" width={40} height={40} className="my-5" /> */}
+    <div className="bg-slate-50 dark:bg-[#121212] text-gray-800 dark:text-slate-100 h-screen flex flex-col items-center justify-center text-center">
+        <Image src='icon.svg' alt="ToDo App" width={80} height={80} />
         <p className="text-sm font-semibold my-1">Welcome to Todo App</p>
         <p className="text-sm my-1">Log in with your Google account to continue</p>
-        <button className="btn btn-neutral btn-sm text-gray-50"
+        <button className="btn btn-neutral btn-sm mt-5 text-gray-50"
           onClick={() => signIn('google')}
         >
           Log in
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,6 +3,7 @@
 import { signOut, useSession } from "next-auth/react"
 import Image from "next/image"
 import { useState } from "react"
+import { AiFillGithub } from "react-icons/ai"
 import { GiHamburgerMenu } from 'react-icons/gi'
 
 function NavBar() {
@@ -10,7 +11,7 @@ function NavBar() {
   const { data: session } = useSession();
 
   return (
-    <div className="h-12 w-full flex flex-row justify-between px-4 items-center bg-gray-100 dark:bg-[#121212] rounded-b-lg shadow-lg dark:shadow-[#4a4a4a1f]">
+    <div className="relative h-12 w-full flex flex-row justify-between px-4 items-center bg-gray-100 dark:bg-[#121212] rounded-b-lg shadow-lg dark:shadow-[#4a4a4a1f]">
         <div className='flex flex-row items-center text-2xl font-sans font-bold text-gray-800'>
           <Image src = 'icon.svg' width={32} height={32} alt="icon" />
           {/* Todo App */}
@@ -21,7 +22,7 @@ function NavBar() {
           <GiHamburgerMenu className='h-6 w-6 fill-gray-600 dark:fill-gray-300' />
         </button>
         {showMenu && 
-        <div className='absolute top-[64px] right-[10px] flex flex-col items-center rounded-md w-[120px] h-fit shadow-lg dark:shadow-[#4a4a4a1f] bg-slate-100 dark:bg-[#121212] z-999'>
+        <div className='absolute top-[64px] right-[10px] flex flex-col items-center rounded-md w-[120px] h-fit shadow-lg dark:shadow-[#4a4a4a1f] backdrop-blur-2xl'>
           <div>
             {session && 
             <img src={session?.user?.image!} width={64} height={64} className='mt-4 rounded-full hover:cursor-pointer z-999' />
@@ -30,11 +31,16 @@ function NavBar() {
           <div className='flex justify-center items-center text-sm font-semibold text-gray-800 dark:text-slate-50 z-999'>
             {session && session?.user?.name}
           </div>
-          <button className='btn btn-neutral btn-sm text-gray-50 text-sm my-5 z-999'
+          <button className='btn btn-neutral btn-sm text-gray-50 text-sm my-5'
             onClick={() => {signOut()}}
           >
             Logout
           </button>
+          <a href='https://github.com/Ansh-Kushwaha/todo-app' target='_blank'
+            className='btn btn-accent btn-sm text-gray-50 text-xs my-2'
+          >
+            <AiFillGithub /> Project
+          </a>
         </div>
         }
     </div>
